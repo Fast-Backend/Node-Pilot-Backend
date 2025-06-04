037dbab4d7fd5b227961cd5197437dc88b4b3fde
Richie911 <oohrichmond.k@gmail.com>
2025-06-04 00:47:49 +0000

    add app.ts

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+import workflowRouter from './routes/workflow.routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Home page');
+});
+app.use('/api', workflowRouter);
+
+export default app;
