2b8edd1ac030ae627294bb99c6cbf747af1a049e
Richie911 <oohrichmond.k@gmail.com>
2025-06-04 01:34:20 +0000

    update workflow mock data

--- src/mock/workflow.models.ts
+++ src/mock/workflow.models.ts
@@ -8,12 +8,6 @@ export const workflows: Workflows =
 
         {
             id: 'wf123',
-            // steps: [
-            //     {
-            //         type: 'controller', name: 'user',
-            //     },
-            //     { type: 'route', name: 'user', path: '/users', method: 'GET' }
-            // ],
             controllers: {
                 name: "user", routes: null
             },
@@ -21,6 +15,23 @@ export const workflows: Workflows =
                 name: "name", type: "string", nullable: true
             }, {
                 name: "age", type: "number", nullable: false
+            }],
+            createdAt: new Date(),
+            updatedAt: new Date()
+        },
+        {
+            id: '123',
+            controllers: {
+                name: "trade", routes: null
+            },
+            props: [{
+                name: "name", type: "string", nullable: true
+            }, {
+                name: "age", type: "number", nullable: false
+            }, {
+                name: "email", type: "string", nullable: false, validation: [{
+                    type: "email"
+                }]
             }],
             createdAt: new Date(),
             updatedAt: new Date()
