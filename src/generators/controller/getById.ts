16ed31271d0e534fa43526d518321a86a12b7e7a
Richie911 <oohrichmond.k@gmail.com>
2025-06-05 03:18:38 +0000

    update getById generator

--- src/generators/controller/getById.ts
+++ src/generators/controller/getById.ts
@@ -1,26 +1,24 @@
 import { capitalize } from "../../utils/helpers";
 
 export const getById = (name: string) => {
-    const modelName = capitalize(name);
-    const singularName = name;
+  const modelName = capitalize(name);
+  const singularName = name;
 
-    const code = `
+  const code = `
 export const get${modelName}ById = async (req: Request, res: Response): Promise<void> => {
   try {
     const { id } = req.params;
 
     if (!id) {
-       res.status(400).json({ message: 'Missing ID parameter' });
-       return;
+      res.status(400).json({ message: 'Missing ID parameter' });
+      return;
     }
 
-    const item = await prisma.${singularName}.findUnique({
-      where: { id },
-    });
+    const item = await ${modelName}Service.getById(id);
 
     if (!item) {
-       res.status(404).json({ message: '${modelName} not found' });
-       return;
+      res.status(404).json({ message: '${modelName} not found' });
+      return;
     }
 
     res.status(200).json(item);
@@ -31,5 +29,5 @@ export const get${modelName}ById = async (req: Request, res: Response): Promise<
 };
 `.trim();
 
-    return code;
+  return code;
 };
