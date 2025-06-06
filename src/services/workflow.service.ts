ac59fd4cec90c98b48a76a2036596e5915f8016d
Richie911 <oohrichmond.k@gmail.com>
2025-06-06 02:37:19 +0000

    update workflow service

--- src/services/workflow.service.ts
+++ src/services/workflow.service.ts
@@ -3,7 +3,6 @@ import path from 'path';
 import { workflows } from '../mock/workflow.models';
 import { generateController } from '../generators/controller.generator';
 import { generateRoute } from '../generators/route.generator';
-import { generateType } from '../generators/type.generator';
 import { generateAppTs } from '../generators/app.generator';
 import { generateServerTs } from '../generators/server.generator';
 import { generateTSConfigWithComments } from '../generators/tsconfig.generator';
@@ -11,6 +10,7 @@ import { generatePrismaService } from '../generators/service.generator';
 import { generateSchemaPrisma } from '../generators/prisma-schema.generator';
 import { generatePrismaClientFile } from '../generators/prisma_lib.generator';
 import { generatePackageJson } from '../generators/package_json.generator';
+import { generateType } from '../generators/type.generator';
 
 export const generateWorkflow = async () => {
     const workflow = workflows.workflows;
@@ -25,11 +25,11 @@ export const generateWorkflow = async () => {
 
     for (const controller of workflow) {
         controllerNames.push(controller.controllers.name);
-        await generateType(controller.controllers.name, baseDir, controller.props)
+        await generateType(controller.controllers.name, baseDir, controller.relations, controller.props)
 
         await generatePrismaService({ modelName: controller.controllers.name, baseDir });
 
-        await generateController(controller.controllers.name, baseDir, controller.props);
+        await generateController(controller.controllers.name, baseDir, controller.props, controller.relations);
 
         await generateRoute(controller.controllers.name, controller.controllers.routes, baseDir);
 
