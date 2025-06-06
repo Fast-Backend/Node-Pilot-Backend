4ea10a241dde8ac69a5299d65ba829989014f0e8
Richie911 <oohrichmond.k@gmail.com>
2025-06-06 02:35:16 +0000

    update controller codegen

--- src/generators/controller.generator.ts
+++ src/generators/controller.generator.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import path from 'path';
 import { capitalize } from '../utils/helpers';
-import { Properties } from '../types/workflow';
+import { Properties, Relation } from '../types/workflow';
 import { createNew } from './controller/createNew';
 import { getAll } from './controller/getAll';
 import { getById } from './controller/getById';
@@ -9,11 +9,11 @@ import { update } from './controller/update';
 import { deleteById } from './controller/delete';
 import pluralize from 'pluralize';
 
-export const generateController = async (name: string, baseDir: string, properties?: Properties[]) => {
+export const generateController = async (name: string, baseDir: string, properties?: Properties[], relations?: Relation[]) => {
   const controllerName = pluralize(capitalize(name));
   const allData = pluralize(name);
   const getAllfunc = getAll(name, controllerName, allData);
-  const createFunc = createNew(name, properties)
+  const createFunc = createNew(name, properties, relations)
   const getByIdFunc = getById(name);
   const updateByIdFunc = update(name, properties)
   const deleteByIdFunc = deleteById(name);
@@ -22,6 +22,8 @@ import { Request, Response } from 'express';
 ${createFunc && `import { z } from 'zod';
 import { ${capitalize(name)}Type } from "../types/${name}";
 import {${capitalize(name)}Service } from '../services/${name}.service';
+${relations && `import prisma from '../lib/prisma';`}
+
 `}
   ${getAllfunc}
   ${createFunc}
