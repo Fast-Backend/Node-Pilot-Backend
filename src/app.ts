acf48a229ca3fc02630c558944788cd8a7e4f20c
Richie911 <oohrichmond.k@gmail.com>
2025-06-12 16:55:03 +0000

    update origins

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,9 @@ import workflowRouter from './routes/workflow.routes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*"
+}));
 app.use(express.json());
 app.get('/', (req, res) => {
     res.send('Home page');
