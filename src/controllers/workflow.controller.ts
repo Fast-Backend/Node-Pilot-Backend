48575f1794c189e38cbc993f2ca531fdad5334bd
Richie911 <oohrichmond.k@gmail.com>
2025-06-12 16:09:31 +0000

    Update workflow controller

--- src/controllers/workflow.controller.ts
+++ src/controllers/workflow.controller.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from 'express';
 import { generateWorkflow } from '../services/workflow.service';
+import { Workflows } from '../types/workflow';
 
-export const handleWorkflowGenerate = async (req: Request, res: Response) => {
+export const handleWorkflowGenerate = async (req: Request<{}, {}, Workflows>, res: Response) => {
+  const data = req.body
   try {
-    const result = await generateWorkflow();
+    const result = await generateWorkflow(data);
     res.status(200).json(result);
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
